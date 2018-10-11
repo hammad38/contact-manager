d436cfa10bd182181e5442699d27504587620a9f
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-11 21:43:10 +0200

    Updates

---
+++ precache-manifest.a39c23b6c46db072f6cc168ed69ee43a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "1e178e137667d860b47a",
+    "url": "/contact-manager/static/js/main.1e178e13.chunk.js"
+  },
+  {
+    "revision": "33ccb93b16bef2b55cdb",
+    "url": "/contact-manager/static/js/1.33ccb93b.chunk.js"
+  },
+  {
+    "revision": "1e178e137667d860b47a",
+    "url": "/contact-manager/static/css/main.a9570715.chunk.css"
+  },
+  {
+    "revision": "33ccb93b16bef2b55cdb",
+    "url": "/contact-manager/static/css/1.d6c3b1d1.chunk.css"
+  },
+  {
+    "revision": "64591c97f9e5a07dfa3d71704b7d114b",
+    "url": "/contact-manager/index.html"
+  }
+];
