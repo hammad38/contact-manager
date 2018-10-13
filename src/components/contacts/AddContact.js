104d8ef74a4b682f8c7d63f8660ade3597eaceeb
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-13 02:30:46 +0200

    add delete working without api call but using redux

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
 import TextInputGroup from '../layout/TextInputGroup';
+import {addContact} from "../../actions/contactActions";
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -31,12 +35,13 @@ class AddContact extends Component {
     }
 
     const newContact = {
+      id: uuid(),
       name,
       email,
       phone
     };
 
-    //// SUBMIT CONTACT ////
+    this.props.addContact(newContact);
 
     // Clear State
     this.setState({
@@ -96,4 +101,8 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+AddContact.propTyes = {
+  addContact: PropTypes.func.isRequired
+}
+
+export default connect(null, {addContact})(AddContact);
