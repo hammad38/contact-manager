4f1a463f947e395ffc084a14d8c28b786e4069f2
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-13 02:54:35 +0200

    add delete working using api

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,21 +1,41 @@
 import {GET_CONTACTS, DELETE_CONTACTS, ADD_CONTACTS} from "./types";
+import axios from 'axios';
 
-export const getContacts = () => {
-  return {
-    type: GET_CONTACTS
+export const getContacts = () => async dispatch => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  dispatch(
+    {
+      type: GET_CONTACTS,
+      payload: response.data
+    }
+  );
+}
+
+export const deleteContact = (id) => async dispatch => {
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch(
+      {
+        type: DELETE_CONTACTS,
+        payload: id
+      }
+    );
+  } catch (e) {
+    dispatch(
+      {
+        type: DELETE_CONTACTS,
+        payload: id
+      }
+    );
   }
 }
 
-export const deleteContact = (id) => {
-  return {
-    type: DELETE_CONTACTS,
-    payload: id
-  }
-}
-
-export const addContact = (newContact) => {
-  return {
-    type: ADD_CONTACTS,
-    payload: newContact
-  }
+export const addContact = (newContact) => async dispatch => {
+  const response = await axios.post('https://jsonplaceholder.typicode.com/users/', newContact);
+  dispatch(
+    {
+      type: ADD_CONTACTS,
+      payload: response.data
+    }
+  );
 }
--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -3,7 +3,6 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import TextInputGroup from '../layout/TextInputGroup';
 import {addContact} from "../../actions/contactActions";
-import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
@@ -35,7 +34,6 @@ class AddContact extends Component {
     }
 
     const newContact = {
-      id: uuid(),
       name,
       email,
       phone
